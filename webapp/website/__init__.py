fefb5a9e637760aa9c3d0bae3fbf3d2b1f2c9591
yinan-you <136161039+yinan-you@users.noreply.github.com>
2024-12-11 21:53:21 +1100

    upgrade pip

--- webapp/website/__init__.py
+++ webapp/website/__init__.py
@@ -8,6 +8,7 @@ def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'peepeepoopoo'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite///{DB_NAME}'
+    db.init_app(app)
 
     from .views import views
     from .eoi import eoi
