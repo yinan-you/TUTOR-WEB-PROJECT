8d6a4b6e4b5d815e151d2fc0364b07dddf596552
yinan-you <136161039+yinan-you@users.noreply.github.com>
2024-12-11 21:30:09 +1100

    new venv

--- webapp/website/__init__.py
+++ webapp/website/__init__.py
@@ -1,8 +1,13 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'peepeepoopoo'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite///{DB_NAME}'
 
     from .views import views
     from .eoi import eoi
