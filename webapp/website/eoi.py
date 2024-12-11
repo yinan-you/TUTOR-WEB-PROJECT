6d42512d7e46a229f78bd4ad2804fe2a9f1e21c2
yinan-you <136161039+yinan-you@users.noreply.github.com>
2024-12-11 16:37:54 +1100

    Update code version
    issues involving version updates for flash messages

--- webapp/website/eoi.py
+++ webapp/website/eoi.py
@@ -1,8 +1,34 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 eoi = Blueprint('eoi', __name__)
 
 
-@eoi.route('/sign-up')
+@eoi.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstname = request.form.get('firstName')
+        
+        if len(email) < 4:
+            flash('idiot', category='error')
+        elif len(firstname) < 2:
+            flash('idiot', category='error')
+        else:
+            # add user to database
+            flash('non-idiot', category='success')
     return render_template("sign_up.html", bool=True)
+
+@eoi.route('/sign-up', methods=['GET', 'POST'])
+def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstname = request.form.get('firstName')
+        
+        if len(email) < 4:
+            flash('Email must be at least 4 characters long.', category='error')
+        elif len(firstname) < 2:
+            flash('First name must be at least 2 characters long.', category='error')
+        else:
+            # Add user to database logic here
+            flash('Successfully signed up!', category='success')
+    return render_template("sign_up.html")
