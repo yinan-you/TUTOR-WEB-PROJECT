6fb4144b75a0e42eebaa18d335522ba5e2fadbea
yinan-you <136161039+yinan-you@users.noreply.github.com>
2025-01-12 11:46:30 +1100

    Fixed ootstrap

--- webapp/website/views.py
+++ webapp/website/views.py
@@ -4,4 +4,4 @@ views = Blueprint('views', __name__)
 
 @views.route('/') # whenever we go to / route, we will run this. This is the homepage
 def home():
-    return render_template("home2.html")
+    return render_template("home.html")
