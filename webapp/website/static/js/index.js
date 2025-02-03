04de6d700541b0fcee78f6ac14ce71654f469fed
yinan-you <136161039+yinan-you@users.noreply.github.com>
2025-02-03 17:09:27 +1100

    Minor experimental animations
    -GSAP scroll trigger option, but have not added to the html
    -Sliding animation with keyframes, also have not added in

---
+++ webapp/website/static/js/index.js
@@ -0,0 +1,11 @@
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.from(".box", {
+  opacity: 0,
+  y: 100,
+  duration: 1,
+  scrollTrigger: {
+    trigger: ".box",
+    start: "top 80%", // Starts animation when top of .box reaches 80% of viewport
+    end: "top 30%", // Ends at 30% of viewport
+    scrub: true, // Smoot
