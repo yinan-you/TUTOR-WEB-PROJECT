f30224eedc2c5ec7ab1d65b03a43a6ffe4576be3
yinan-you <136161039+yinan-you@users.noreply.github.com>
2025-02-03 19:34:13 +1100

    more code rendered useless
    eoi libary deleted

--- webapp/website/__init__.py
+++ webapp/website/__init__.py
@@ -13,11 +13,9 @@ def create_app():
     db.init_app(app)
 
     from .views import views
-    from .eoi import eoi
     from .join_front_page import join_front_page
 
     app.register_blueprint(views, url_prefix='/')
-    app.register_blueprint(eoi, url_prefix='/')
     app.register_blueprint(join_front_page, url_prefix='/')
 
     from website.models import User, Note
