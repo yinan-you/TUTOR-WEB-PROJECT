d40807372900fc1e6fc37c659b415ed9c39f79ad
yinan-you <136161039+yinan-you@users.noreply.github.com>
2025-02-03 22:00:59 +1100

    scroll circle
    also added parallex wrapper code and css

--- webapp/website/static/js/index.js
+++ webapp/website/static/js/index.js
@@ -11,3 +11,26 @@ gsap.from(".box", {
     scrub: true, // Smooth animation while scrolling
   },
 });
+
+gsap.to(".background", {
+  yPercent: 50, // Moves the background down 50% as you scroll
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".parallax-wrapper",
+    start: "top top", // Start when the top of the parallax container reaches the top of the viewport
+    end: "bottom top", // End when the bottom of the parallax container reaches the top of the viewport
+    scrub: true, // Smooth scroll effect
+  },
+});
+
+gsap.to(".scroll-circle", {
+  scrollTrigger: {
+    trigger: ".scroll-circle", // Element that triggers the animation
+    start: "top 80%", // When the top of the trigger hits 80% of the viewport height
+    end: "top 20%", // Ends the animation when the top of the trigger hits 20%
+    scrub: true, // Makes the animation smooth and linked to the scroll
+    markers: true, // Optional, shows markers for debugging
+  },
+  y: 200, // Moves the circle 200px along the Y-axis
+  ease: "power1.inOut",
+});
